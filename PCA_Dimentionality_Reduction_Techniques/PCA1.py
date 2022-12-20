dd680e4c79d2595581919c9a70355f66fdd45299
Divya Mereddy <37320170+DivyaMereddy007@users.noreply.github.com>
2022-12-19 22:32:49 -0600

    commit pca

---
+++ PCA_Dimentionality_Reduction_Techniques/PCA1.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+
+#import the breast _cancer dataset
+from sklearn.datasets import load_breast_cancer
+data=load_breast_cancer()
+data.keys()
+
+# Check the output classes
+print(data['target_names'])
+
+# Check the input attributes
+print(data['feature_names'])
+
+
+#Apply PCA
+
+
+# construct a dataframe using pandas
+df1=pd.DataFrame(data['data'],columns=data['feature_names'])
+
+# Scale data before applying PCA
+scaling=StandardScaler()
+
+# Use fit and transform method
+scaling.fit(df1)
+Scaled_data=scaling.transform(df1)
+
+# Set the n_components=3
+principal=PCA(n_components=3)
+principal.fit(Scaled_data)
+x=principal.transform(Scaled_data)
+
+# Check the dimensions of data after PCA
+print(x.shape)
+
+
+#Check Components
+
+principal.components_
+#5. Plot the components (Visualization)
+
+#2d
+plt.figure(figsize=(10,10))
+plt.scatter(x[:,0],x[:,1],c=data['target'],cmap='plasma')
+plt.xlabel('pc1')
+plt.ylabel('pc2')
+
+
+
+
+#Calculate variance ratio
+# check how much variance is explained by each principal component
+print(principal.explained_variance_ratio_)
